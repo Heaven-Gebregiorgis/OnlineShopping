46ee43a4cafe2e9a47033584ea5a84ab9d01b485
Heaven-Gebregiorgis <105028237+Heaven-Gebregiorgis@users.noreply.github.com>
2022-05-09 22:29:32 -0700

    Add files via upload

--- src/main/java/com/solvd/OnlineShopping/domparser/DOMParsing.java
+++ src/main/java/com/solvd/OnlineShopping/domparser/DOMParsing.java
@@ -1,105 +1,71 @@
 package com.solvd.OnlineShopping.domparser;
 
 import java.io.File;
+import java.io.IOException;
+
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 
 
 public class DOMParsing {
 	
-	public static void main(String[] args) {
-    try {
-       File inputFile = new File("/Users/apple/Desktop/Solvd automation/OnlineShopping/src/main/resources/DOMInput.xml");
-       DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-       DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-       Document doc = dBuilder.parse(inputFile);
-       doc.getDocumentElement().normalize();
-       System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-       NodeList nList = doc.getElementsByTagName("Vendors");
-       System.out.println("----------------------------");
-       
-       for (int temp = 0; temp < nList.getLength(); temp++) {
-          Node nNode = nList.item(temp);
-          System.out.println("\nCurrent Element :" + nNode.getNodeName());
-          
-          if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-             Element eElement = (Element) nNode;
-             System.out.println("Vendor Id " 
-            		 + eElement.getAttribute("id"));
-             System.out.println("Vendor Name : " 
-                + eElement
-                .getElementsByTagName("vendorName")
-                .item(0)
-                .getTextContent());
-             System.out.println("Contact Name : " 
-                + eElement
-                .getElementsByTagName("contactName")
-                .item(0)
-                .getTextContent());
-             System.out.println("Email : " 
-                + eElement
-                .getElementsByTagName("email")
-                .item(0)
-                .getTextContent());
-             System.out.println("Phone : " 
-                + eElement
-                .getElementsByTagName("phone")
-                .item(0)
-                .getTextContent());
-             for (int j = 0; j < nList.getLength(); j++) {
-                 Node nNode1 = nList.item(j);
-                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                     Element eElement1 = (Element) nNode1;
-                     System.out.println("Address Id " 
-                    		 + eElement1.getAttribute("id"));
-                     System.out.println("House Number : " 
-                        + eElement1
-                        .getElementsByTagName("houseNumber")
-                        .item(0)
-                        .getTextContent());
-                     System.out.println("Street : " 
-                        + eElement1
-                        .getElementsByTagName("street")
-                        .item(0)
-                        .getTextContent());
-                     System.out.println("Apartment Number : " 
-                        + eElement1
-                        .getElementsByTagName("apartmentNumber")
-                        .item(0)
-                        .getTextContent());
-                     System.out.println("City : " 
-                        + eElement1
-                        .getElementsByTagName("city")
-                        .item(0)
-                        .getTextContent());
-                     System.out.println("State : " 
-                             + eElement1
-                             .getElementsByTagName("state")
-                             .item(0)
-                             .getTextContent());
-                          System.out.println("Zip Code : " 
-                             + eElement1
-                             .getElementsByTagName("postalCode")
-                             .item(0)
-                             .getTextContent());
-                          System.out.println("Country : " 
-                             + eElement1
-                             .getElementsByTagName("country")
-                             .item(0)
-                             .getTextContent());
-          
-       }
-             }
-          }
-    }
-    }catch (Exception e) {
-       e.printStackTrace();
-    }
- }
+	
+		   public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException
+		   {
+		      //Get Document Builder
+		      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		      DocumentBuilder builder = factory.newDocumentBuilder();
+		       
+		      //Build Document
+		      Document document = builder.parse(new File("/Users/apple/Desktop/Solvd automation/OnlineShopping/src/main/resources/DOMInput.xml"));
+		       
+		      //Normalize the XML Structure; It's just too important !!
+		      document.getDocumentElement().normalize();
+		       
+		      //Here comes the root node
+		      Element root = document.getDocumentElement();
+		      System.out.println(root.getNodeName());
+		       
+		      //Get all employees
+		      NodeList nList = document.getElementsByTagName("Vendor");
+		      System.out.println("============================");
+		       
+		      visitChildNodes(nList);
+		   }
+		 
+		   //This function is called recursively
+		   private static void visitChildNodes(NodeList nList)
+		   {
+		      for (int temp = 0; temp < nList.getLength(); temp++)
+		      {
+		         Node node = nList.item(temp);
+		         if (node.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            System.out.println("Node Name = " + node.getNodeName() + "; Value = " + node.getTextContent());
+		            //Check all attributes
+		            if (node.hasAttributes()) {
+		               // get attributes names and values
+		               NamedNodeMap nodeMap = node.getAttributes();
+		               for (int i = 0; i < nodeMap.getLength(); i++)
+		               {
+		                   Node tempNode = nodeMap.item(i);
+		                   System.out.println("Attr name : " + tempNode.getNodeName()+ "; Value = " + tempNode.getNodeValue());
+		               }
+		               if (node.hasChildNodes()) {
+		                  //We got more childs; Let's visit them as well
+		                  visitChildNodes(node.getChildNodes());
+		               }
+		           }
+		         }
+		      }
+		   }
 }
 
 
